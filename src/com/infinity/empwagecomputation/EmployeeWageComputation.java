af7813d1d1ef35419a7f415cc6ae573c29e0e045
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-04-21 16:32:36 +0530

    [ADDED]:[Vikas] Employee Wage Computation Project status with initial position

---
+++ src/com/infinity/empwagecomputation/EmployeeWageComputation.java
@@ -0,0 +1,11 @@
+package com.infinity.empwagecomputation;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Wage Computation");
+
+	}
+
+}
