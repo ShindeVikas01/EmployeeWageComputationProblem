0456ce0ef4dc406a94926dbfaefe3fecaa9bad18
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-04-21 16:49:42 +0530

    [MODIFIED]:[Vikas] Check Employee is Present or Absent

--- src/com/infinity/empwagecomputation/EmployeeWageComputation.java
+++ src/com/infinity/empwagecomputation/EmployeeWageComputation.java
@@ -1,11 +1,24 @@
 package com.infinity.empwagecomputation;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation");
-
+		
+		/***
+		 * Random class object use for employee is present or absent
+		 */
+		Random random=new Random();
+		
+		int empStatus=random.nextInt(2);
+		if(empStatus==1){
+			System.out.println("Employee is Present");
+		}else{
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
