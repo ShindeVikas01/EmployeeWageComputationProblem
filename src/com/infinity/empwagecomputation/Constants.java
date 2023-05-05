7e773e38867aabea6028738d1540e6fb1c259b1d
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-05-05 10:19:34 +0530

    [MODIFIED]:[Vikas] Modify using class and object methods

---
+++ src/com/infinity/empwagecomputation/Constants.java
@@ -0,0 +1,17 @@
+package com.infinity.empwagecomputation;
+
+public class Constants {
+	/**
+	 * WAGE_PER_HOUR use for per hour wage
+	 * FULL_DAY_HOUR use for full time hours 
+	 * PART_TIME_HOUR use for part time hours
+	 * IS_PART_TIME use for employee is part time present
+	 * IS_FULL_TIME use for employee is full time present
+	 */
+
+	final static int IS_PRESENT=1;
+	final static int FULL_TIME_HOUR=8;
+	final static int PART_TIME_HOUR=4;
+	final static int IS_PART_TIME=0;
+	final static int IS_FULL_TIME=1;
+}
